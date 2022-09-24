1eb0dac277dfc23366ba3982a61915b51fc7a3e2
Don Franklie <donfranklie@gmail.com>
2022-09-24 07:58:18 +0300

    Changed their Location

--- script.js
+++
@@ -1,9 +0,0 @@
-// I WILL LOOK AT THIS LATER
-
-// const hamburger = document.querySelector(".hamburger");
-// const navMenu = document.querySelector(".nav-menu");
-
-// hamburger.addEventListener("click", () => {
-//     hamburger.classList.toggle("active");
-//     navMenu.classList.toggle("active");
-// })
