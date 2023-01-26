8c75721a7d0517d8c88eb3aea47eec1c9be4615d
DonFranklie <107101640+DonFranklie@users.noreply.github.com>
2023-01-26 17:54:44 +0300

    Hamburger Menu with JS

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-function toggleMenu() {
-    var menuItems = document.querySelector('.menu-items');
-    menuItems.classList.toggle('menu-open');
+let harmburgerMenu = document.querySelector('.hamburger-menu');
+let menuList = document.querySelector('.menu-list');
+
+function toggle(){
+ 
+//  menuList.style.display = 'block';
+  if(menuList.style.left == '-1000px'){
+    menuList.style.left = '0'
+  }else{
+    menuList.style.left = '-1000px';
   }
+}
   
